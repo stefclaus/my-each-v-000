664cff274cb67c503da7b0ff0e55e2f1f29a7e8b
Stef Claus <stef.claus@gmail.com>
2018-09-15 16:57:50 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,18 @@
-def my_each # put argument(s) here
+def my_each(collection)
   # code here
+end
+
+def hello_t(array)
+  if block_given?
+    i = 0
+ 
+  while i < array.length
+    yield array[i]
+    i = i + 1
+  end
+  
+  array
+  else 
+    puts "Hey! No block was given!"
+  end 
 end
